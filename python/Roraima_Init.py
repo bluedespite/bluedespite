230d19178c98f8d80e1a894503d291d4f033f8bd
Miguel Aguirre <miguelaguirreleon@gmail.com>
2020-11-24 18:59:49 -0500

    Modificacion de leer archivo a configuracion por input de usuario

--- python/Roraima_Init.py
+++ python/Roraima_Init.py
@@ -4,52 +4,166 @@
 import mysql.connector
 import csv
 
-
-
-
-csv_configuracion=open('CONFIGURACION.csv')
-csv_data=csv.reader(csv_configuracion, delimiter=';')
-reg=next(csv_data)
-reg=next(csv_data)
-#Crear Tabla con nombre tag_CONFIGi
-TAG_SENSOR=reg[1]
 connection=mysql.connector.connect (host='localhost',database='MAIN_SENSOR',user='admin',password='12345')
 cursor=connection.cursor()
+print("***Configuracion Inicial***")
+while True:
+    n=input("Seleccione si es la Primera vez que corre la inicializacion(S/n):")
+    if n=="S":
+        sql_select_Query= "CREATE OR REPLACE TABLE `MAIN` ( `ID` INT NOT NULL , `DB_SENSOR` TEXT NOT NULL , INDEX `ID` (`ID`)) ENGINE = InnoDB"
+        cursor.execute(sql_select_Query)
+        break
+    if n=="n":
+        break
 
-sql_select_Query= "CREATE OR REPLACE TABLE `MAIN` ( `ID` INT NOT NULL , `DB_SENSOR` TEXT NOT NULL , INDEX `ID` (`ID`)) ENGINE = InnoDB"
-#ATENCION!!!: SOLO USAR LA SIGUIENTE LINEA LA PRIMERA VEZ QUE SE CARGA UN SENSOR, LA SEGUNDA VEZ HAY QUE COMENTAR
+print("+++Configuracion del Sensor +++")
+TAG_SENSOR=input("Nombre del Sensor:")
+sql_select_Query= "CREATE OR REPLACE TABLE `MAIN_SENSOR`.`"+ TAG_SENSOR + "_CONF` ( `ID` INT NOT NULL , `TAG` TEXT NOT NULL ,`DESCRIPCION` TEXT NOT NULL , `UNITS` TEXT NOT NULL , `RANG_MIN` TEXT NOT NULL , `RANG_MAX` TEXT NOT NULL, `PROTOCOLO` TEXT NOT NULL,`DIRECCION` TEXT NOT NULL, `PARAM_COMM1` TEXT NOT NULL, `PARAM_COMM2` TEXT NOT NULL,`PARAM_COMM3` TEXT NOT NULL,`PARAM_COMM4` TEXT NOT NULL,`PARAM_COMM5` TEXT NOT NULL,`PARAM_COMM6` TEXT NOT NULL,   INDEX `ID` (`ID`)) ENGINE = InnoDB"
 cursor.execute(sql_select_Query)
-
-sql_select_Query= "CREATE OR REPLACE TABLE `MAIN_SENSOR`.`"+ TAG_SENSOR + "_CONF` ( `ID` INT NOT NULL , `PARAMETRO` TEXT NOT NULL , `VALOR` TEXT NOT NULL , `DESCRIPCION` TEXT NOT NULL , INDEX `ID` (`ID`)) ENGINE = InnoDB"
-cursor.execute(sql_select_Query)
-i=1
-index=str(i)
-
-sql_select_Query= "INSERT INTO `"+ TAG_SENSOR + "_CONF` (`ID`, `PARAMETRO`, `VALOR`, `DESCRIPCION`) VALUES ("+ index +",'"+ reg[0] +"','"+ reg[1] +"','"+ reg[2] +"')"
-cursor.execute(sql_select_Query)
-DATOS_COM=['FECHA_HORA']
-
-for reg in csv_data:
-    i=i+1
-    index=str(i)
-    sql_select_Query= "INSERT INTO `"+ TAG_SENSOR + "_CONF` (`ID`, `PARAMETRO`, `VALOR`, `DESCRIPCION`) VALUES ("+ index +",'"+ reg[0] +"','"+ reg[1] +"','"+ reg[2] +"')"
+numero=0
+while True:
+    n=input("Introduzca la cantidad de TAGS para el sensor " + TAG_SENSOR+":")
+    try:
+        numero=int(n)
+        if numero>=1:
+            break
+    except:
+        numero=0
+Tags=[]
+for j in range(numero):
+    while True:
+        TAG=input("introduce el tag " + str(j)+":")
+        if TAG!="":
+            Tags.append(TAG)
+            break
+    while True:
+        DESCRIPCION=input(" Introduce la descripcion del tag " +TAG+":")
+        if DESCRIPCION!="":
+            break
+    while True:
+        UNITS=input(" Introduce la unidad de medida del tag " +TAG+":")
+        if UNITS!="":
+            break
+    while True:
+        RANG_MIN=input (" Introduce el Rango minimo:")
+        RANG_MAX=input (" Introduce el Rango maximo:")
+        try:
+            n1=float(RANG_MIN)
+            n2=float(RANG_MAX)
+            if n2>n1:
+                break
+        except:
+            n1=0;
+            n2=0;
+    while True:
+        TIPO=input(" Introduce el Tipo de Comunicacion (ANA=Analogica),(MTCP=Modbus TCP), (MSER=Modus Serial):")
+        if TIPO=="ANA" :
+            while True:
+                DIRECCION=input("Introduzca el Canal Analogico (1-5) (canal test:0):")
+                try:
+                    n1=int(DIRECCION)
+                    if n1>=0 and n1<6:
+                        break
+                except:
+                    n1=0
+            PARAM_COMM1="0"
+            PARAM_COMM2="0"
+            PARAM_COMM3="0"
+            PARAM_COMM4="0"
+            PARAM_COMM5="0"
+            PARAM_COMM6="0"
+            break
+        if TIPO=="MTCP":
+            while True:
+                DIRECCION=input("Introduzca la direccion IP del sensor .eje. xx.xx.xx.xx:")
+                try:
+                    nn=DIRECCION.split(".")
+                    numero=int(nn[0])+int(nn[1])+int(nn[2])+int(nn[3])
+                    if numero>1:
+                        break
+                except:
+                    numero=0
+            while True:
+                PARAM_COMM1=input("Introduzca la mascara IP  ej 255.255.255.0:")
+                try:
+                    nn=PARAM_COMM1.split(".")
+                    numero=int(nn[0])+int(nn[1])+int(nn[2])+int(nn[3])
+                    if numero>1:
+                        break
+                except:
+                    numero=-1
+            while True:
+                PARAM_COMM2=input("Introduzca el Gateway: ej. xxx.xxx.xxx.xxx :")
+                try:
+                    nn=PARAM_COMM2.split(".")
+                    numero=int(nn[0])+int(nn[1])+int(nn[2])+int(nn[3])
+                    if numero>1:
+                        break
+                except:
+                    numero=0
+            while True:
+                PARAM_COMM3=input("Introduzca el puerto de Comunicaciones: ej. 502: ")
+                try:
+                    numero=int(PARAM_COMM3)
+                    if numero>=1:
+                        break
+                except:
+                    numero=0
+            while True:
+                PARAM_COMM4=input("Introduzca la ID MODBUS: ej. 1: ")
+                try:
+                    numero=int(PARAM_COMM4)
+                    if numero>=0:
+                        break
+                except:
+                    numero=-1
+            while True:
+                PARAM_COMM5=input("Introduzca direccion MODBUS de la medida: ej. 40020: ")
+                try:
+                    numero=int(PARAM_COMM5)
+                    if numero>40000:
+                        break
+                except:
+                    numero=0
+            PARAM_COMM6="0"
+            break
+    sql_select_Query= "INSERT INTO `"+ TAG_SENSOR + "_CONF` (`ID`, `TAG`, `DESCRIPCION`,`UNITS`,`RANG_MIN`,`RANG_MAX`,`PROTOCOLO`,`DIRECCION`,`PARAM_COMM1`,`PARAM_COMM2`,`PARAM_COMM3`,`PARAM_COMM4`,`PARAM_COMM5`,`PARAM_COMM6`) VALUES ("+ str(j+1) +",'"+ TAG +"','"+ DESCRIPCION +"','"+ UNITS +"','"+ RANG_MIN +"','"+ RANG_MAX +"','"+ TIPO +"','"+ DIRECCION +"','"+PARAM_COMM1 +"','"+ PARAM_COMM2 +"','"+ PARAM_COMM3 +"','"+ PARAM_COMM4 +"','"+ PARAM_COMM5 +"','"+ PARAM_COMM6 +"')"
     cursor.execute(sql_select_Query)
-    if i>6:
-        DATOS_COM.append(reg[1])
+    while True:
+        TABLA=input("Introduzca cantidad de puntos en tabla de linealizacion: ")
+        try:
+            numero=int(TABLA)
+            if numero<2:
+                TABLA="2"
+                numero=2
+            break
+        except:
+            numero=0
+    print("***Tabla de linealizacion de"+TABLA+" puntos en pares (Xn,Yn)***")
+    x=[]
+    y=[]
+    sql_select_Query="CREATE OR REPLACE TABLE `MAIN_SENSOR`.`"+ TAG + "_TABLA` ( `ID` INT NOT NULL , `ENTRADA` TEXT NOT NULL ,`SALIDA` TEXT NOT NULL ,   INDEX `ID` (`ID`)) ENGINE = InnoDB"
+    cursor.execute(sql_select_Query)
+    for n in range(numero):
+        while True:
+            x.append(input("Introduzca la medida X"+str(n)+" : "))
+            y.append(input("Introduzca la medida Y"+str(n)+" : "))
+            try:
+                numero=int(x[n])+int(y[n])
+                if numero>=0:
+                    sql_select_Query= "INSERT INTO `"+ TAG + "_TABLA` (`ID`, `ENTRADA`, `SALIDA`) VALUES ("+ str(n+1) +",'"+ x[n] +"','"+ y[n] +"')"
+                    cursor.execute(sql_select_Query)
+                    break
+            except:
+                numero=-1
 
-connection.commit()
 
-sql_select_Query= "CREATE OR REPLACE TABLE `MAIN_SENSOR`.`"+ TAG_SENSOR + "_MEASURE` ( `ID` INT NOT NULL ,"
+sql_select_Query="CREATE OR REPLACE TABLE `MAIN_SENSOR`.`"+ TAG_SENSOR + "_MEASURE` ( `ID` INT NOT NULL ,`FECHA_HORA` DATETIME NOT NULL"
+for n in Tags:
+    sql_select_Query= sql_select_Query + ", `" + n + "` FLOAT NOT NULL"
 
-for n in DATOS_COM:
-    sql_select_Query= sql_select_Query + "`" + n + "` FLOAT NOT NULL,"
-
-j=len(sql_select_Query)
-sql_select_Query=sql_select_Query[:j-1]
 sql_select_Query=sql_select_Query + "  , INDEX `ID` (`ID`)) ENGINE = InnoDB"
-
 cursor.execute(sql_select_Query)
-cursor.execute("ALTER TABLE `"+TAG_SENSOR+"_MEASURE` CHANGE `FECHA_HORA` `FECHA_HORA` DATETIME NOT NULL");
 
 sql_select_Query = ("SELECT * FROM MAIN " "WHERE DB_SENSOR = '"+TAG_SENSOR+"'")
 cursor.execute(sql_select_Query)
@@ -69,4 +183,3 @@ if i==0:
     connection.commit()
 
 connection.close()
-csv_configuracion.close()
