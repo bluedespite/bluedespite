4b804c2ab83934b6b75e9325b59c5471f42ea3b7
mikeatthebass <miguelaguirreleon@gmail.com>
2021-05-10 21:11:27 -0500

    m

--- python/Roraima_server.py
+++ python/Roraima_server.py
@@ -25,21 +25,21 @@ def Roraima_Server():
         except:
             logging.error("No se puede contectar a base de datos main_server del servidor central")
             #break
-        for direccion in LISTA_DIRECCIONES[:][0]:
+        for direccion in LISTA_DIRECCIONES:
             error_general=0;
             try:
-                connection_remoto=mysql.connector.connect (host=direccion ,database='MAIN_SENSOR',user='remoto',password='12345')
+                connection_remoto=mysql.connector.connect (host=direccion[:][0] ,database='MAIN_SENSOR',user='remoto',password='12345')
                 cursor_remoto=connection_remoto.cursor()
                 cursor_remoto.execute("SELECT DB_SENSOR FROM MAIN WHERE 1")
                 LISTA_SENSORES=cursor_remoto.fetchall()
             except:
-                logging.error("No se puede contectar a base de datos main_server del servidor: "+str(direccion))
+                logging.error("No se puede contectar a base de datos main_server del servidor: "+str(direccion[:][0]))
                 error_general=1
             j=0
             if error_general==0:
-                for SENSOR in LISTA_SENSORES[:][0]:
+                for SENSOR in LISTA_SENSORES:
                     j=j+1
-                    Query="SELECT * FROM "+ SENSOR +"_CONF WHERE 1"
+                    Query="SELECT * FROM "+ SENSOR[:][0] +"_CONF WHERE 1"
                     cursor_local.execute(Query)
                     CONF = cursor_local.fetchall()
                     CANT_DIR=len(CONF)
@@ -55,26 +55,26 @@ def Roraima_Server():
                     PARAM_COMM5=[T[12] for T in CONF]
                     PARAM_COMM6=[T[13] for T in CONF]
                     flag=False
-                    prueba=SENSOR+"_MEASURE"
-                    for DB in DBS[:][0]:
-                        if DB[0] == prueba:
+                    prueba=SENSOR[:][0]+"_MEASURE"
+                    for DB in DBS:
+                        if DB[:][0] == prueba:
                             flag=True
                     if flag == False:
-                        sql_select_Query="CREATE OR REPLACE TABLE `MAIN_SENSOR`.`"+ SENSOR + "_MEASURE` ( `ID` INT NOT NULL ,`FECHA_HORA` DATETIME NOT NULL"
+                        sql_select_Query="CREATE OR REPLACE TABLE `MAIN_SENSOR`.`"+ SENSOR[:][0] + "_MEASURE` ( `ID` INT NOT NULL ,`FECHA_HORA` DATETIME NOT NULL"
                         for n in TAGS:
                             sql_select_Query= sql_select_Query + ", `" + n + "` FLOAT NOT NULL"
                         sql_select_Query=sql_select_Query + "  , INDEX `ID` (`ID`)) ENGINE = InnoDB"
                         cursor_local.execute(sql_select_Query)
-                    cursor_local.execute("SELECT COUNT(*) FROM MAIN_SENSOR."+ SENSOR +"_MEASURE")
+                    cursor_local.execute("SELECT COUNT(*) FROM MAIN_SENSOR."+ SENSOR[:][0] +"_MEASURE")
                     conteo=cursor_local.fetchone()
                     LAST_ID=0
                     if conteo[0] > 0:
-                        cursor_local.execute("SELECT ID FROM MAIN_SENSOR."+ SENSOR +"_MEASURE ORDER BY ID DESC LIMIT 1")
-                    Query="SELECT * FROM "+ SENSOR +"_MEASURE WHERE `ID` > "+str(LAST_ID)
+                        cursor_local.execute("SELECT ID FROM MAIN_SENSOR."+ SENSOR[:][0] +"_MEASURE ORDER BY ID DESC LIMIT 1")
+                    Query="SELECT * FROM "+ SENSOR[:][0] +"_MEASURE WHERE `ID` > "+str(LAST_ID)
                     cursor_remoto.execute(Query)
                     LAST_ID=cursor_local.fetchone()
                     val=cursor_remoto.fetchall()
-                    Q1="INSERT INTO MAIN_SENSOR."+ SENSOR + "_MEASURE  (`ID`, `FECHA_HORA`"
+                    Q1="INSERT INTO MAIN_SENSOR."+ SENSOR[:][0] + "_MEASURE  (`ID`, `FECHA_HORA`"
                     Q2= ")  VALUES (%s,%s"
                     for t in TAGS:
                         Q1=Q1+ ",`" + t +"`"
