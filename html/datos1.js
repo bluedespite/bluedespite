ab77806de47a008b8563c73891eabf26ce3df0b9
mikeatthebass <miguelaguirreleon@gmail.com>
2021-05-23 22:59:47 -0500

    deb

--- html/datos1.js
+++ html/datos1.js
@@ -9,8 +9,11 @@ $(document).ready(function() {
             var LVL1 = [];
             var LVLWTR= [];
             var TMP= [];
-            var color = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'];
-            var bordercolor = ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
+            var LAT=[];
+            var LON=[];
+            var VEL=[];
+            var color = ['rgba(255, 0, 0, 0.2)', 'rgba(0, 0, 153, 0.2)', 'rgba(0, 102, 0, 0.2)', 'rgba(255, 255, 0, 0.2)', 'rgba(255, 102, 0, 0.2)', 'rgba(102, 0, 153, 0.2)', 'rgba(102, 0, 0, 0.2)'];
+            var bordercolor = ['rgba(255,0,0,1)', 'rgba(0, 0, 153, 1)', 'rgba(0, 102, 0, 1)', 'rgba(255, 255, 0, 1)', 'rgba(255, 102, 0, 1)', 'rgba(102, 0, 153, 1)', 'rgba(0, 0, 0, 1)'];
             console.log(data);
 
             for (var i in data) {
@@ -18,8 +21,11 @@ $(document).ready(function() {
                 LVL1.push(data[i].LVL1);
                 LVLWTR.push(data[i].LVLWTR);
                 TMP.push(data[i].TMP);
+                LAT.push(data[i].LAT);
+                LON.push(data[i].LON);
+                VEL.push(data[i].VEL);
             }
-
+            console.log("value,", TMP);
             var chartdata = {
                 labels: FECHA_HORA,
                 datasets: [
@@ -37,10 +43,17 @@ $(document).ready(function() {
                     yAxisID: 'A'
 
                 },
-                {   label: 'Temperatura',
-                    data: TMP,
+                {   label: 'Velocidad',
+                    data: VEL,
                     borderColor: color[2],
                     backgroundColor: color[2],
+                    yAxisID: 'A'
+
+                },
+                {   label: 'Temperatura',
+                    data: TMP,
+                    borderColor: color[3],
+                    backgroundColor: color[3],
                     yAxisID: 'B'
                 }]};
 
@@ -53,8 +66,8 @@ $(document).ready(function() {
                     responsive: true,
                     scales: {
                         yAxes: [
-                        {id:'A',ticks: {min: 0,max: 10000}},
-                        {id:'B',ticks: {min: 0,max: 1000}}
+                        {id:'A',ticks: {min: 0,max: 100}},
+                        {id:'B',ticks: {min: 0,max: 100}}
 
                             ]
                     }
@@ -62,7 +75,7 @@ $(document).ready(function() {
             });
         },
         error: function(data1) {
-            console.log(data1);
+        console.log(data1);
         }
     });
 });
---
+++ html_1/dashboard.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+
+<?php
+   session_start();
+
+   if(!isset($_SESSION['email'])){
+     header("Location: index.html");
+     exit;
+   }
+?>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Roraima Bussiness Integrator</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <!--mapas de leaffleet-->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header ">
+                <h3>Roraima BI</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>Fuel Bussiness</p>
+                <li class="active">
+                    <a href="#">Resumen</a>
+                </li>
+
+                <li>
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Flotas</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Principal</a>
+                        </li>
+                        <li>
+                            <a href="#">Contrato 001</a>
+                        </li>
+                        <li>
+                            <a href="#">Contrato 002</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Bahias</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">Bahia 001</a>
+                        </li>
+                        <li>
+                            <a href="#">Bahia 002</a>
+                        </li>
+                        <li>
+                            <a href="#">Bahia 003</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Usuarios</a>
+                </li>
+                <li>
+                    <a href="php/logout.php">Logout</a>
+                </li>
+            </ul>
+
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="#" class="download">Download source</a>
+                </li>
+                <li>
+                    <a href="#" class="article">Back to article</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-success">
+                        <i class="fas fa-align-left"></i>
+                        <span>Menu</span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Page</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Page</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Page</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Page</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+			<img src="asset/logo.JPG" alt="Roraima" class="img-fluid" width="100" height="100">
+
+
+			      <h2>Resumen de Stocks</h2>
+            <canvas id="miGrafico"></canvas>
+            <h2>Ruta Recorrida</h2>
+            <div id="mapid" style="width: 600px; height: 400px;"></div>
+            <div class="line"></div>
+
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div class="line"></div>
+
+            <h3>Lorem Ipsum Dolor</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </div>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+
+    <script crossorigin="anonymous" integrity="sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js" integrity="sha256-JG6hsuMjFnQ2spWq0UiaDRJBaarzhFbUxiUTxQDA9Lk=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js" integrity="sha256-XF29CBwU1MWLaGEnsELogU6Y6rcc5nCkhhx89nFMIDQ=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js" integrity="sha256-J2sc79NPV/osLcIpzL3K8uJyAD7T5gaEFKlLDM18oxY=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js" integrity="sha256-CfcERD4Ov4+lKbWbYqXD6aFM9M51gN4GUEtDhkWABMo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="datos1.js"></script>
+
+    <script>
+    	var mymap = L.map('mapid').setView([-12.0632, -77.1126], 13);
+    	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    		maxZoom: 18,
+    		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    		id: 'mapbox/streets-v11',
+    		tileSize: 512,
+    		zoomOffset: -1
+    	}).addTo(mymap);
+
+    	L.marker([-12.0632, -77.1126]).addTo(mymap)
+    	.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+    	var popup = L.popup();
+    	function onMapClick(e) {
+    		popup
+    			.setLatLng(e.latlng)
+    			.setContent("You clicked the map at " + e.latlng.toString())
+    			.openOn(mymap);
+    	}
+    	mymap.on('click', onMapClick);
+    </script>
+
+
+</body>
+
+</html>
