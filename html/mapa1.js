2699c5996c0522abab19a734284d81059e139a86
mikeatthebass <miguelaguirreleon@gmail.com>
2021-05-24 17:07:33 -0500

    nn

--- html/mapa1.js
+++ html/mapa1.js
@@ -18,6 +18,10 @@ $(document).ready(function() {
                 TRYC.push([data[i].LAT,data[i].LON]);
                 PFINAL=[data[i].LAT,data[i].LON];
             }
+            var Vmax=Math.max(VEL);
+            var Vmin=Math.min(VEL);
+            console.log(Vmax);
+            console.log(Vmin);
             var mymap = L.map('mapid').setView(PFINAL, 13);
           	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
           		maxZoom: 18,
