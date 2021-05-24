00f5038781f09287d4ebd2fc2d075f65c46ac82f
mikeatthebass <miguelaguirreleon@gmail.com>
2021-05-24 16:33:09 -0500

    dd

--- html/mapa1.js
+++ html/mapa1.js
@@ -30,8 +30,8 @@ $(document).ready(function() {
           		zoomOffset: -1
           	}).addTo(mymap);
 
-          	L.marker(PFINAL).addTo(mymap).bindPopup("<b>Ubicacion Final</b><br/>").openPopup();
-      	     L.polygon(TRYC).addTo(mymap).bindPopup("Trayectoria Recorrida");
+          	L.marker(PFINAL).addTo(mymap).bindPopup("<b>Ubicacion Final:</b><br/>"+e.latlng.toString()).openPopup();
+      	     L.polygon(TRYC).addTo(mymap).bindPopup("Recorrido:"+e.latlng.toString());
           	var popup = L.popup();
           	function onMapClick(e) {
           		popup
