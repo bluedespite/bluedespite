e634e61ba7123c727ef9f31eb2291cb6a356872f
mikeatthebass <miguelaguirreleon@gmail.com>
2021-05-23 19:07:55 -0500

    dd

--- html_1/dashboard.php
+++ html_1/dashboard.php
@@ -171,7 +171,7 @@
 
 
     <script>
-    	var mymap = L.map('mapid').setView([-9.189967, -75.015152], 10);
+    	var mymap = L.map('mapid').setView([-12.0632, -77.1126], 13);
     	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     		maxZoom: 18,
     		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
