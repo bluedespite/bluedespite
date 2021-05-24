b95b51d34a53890e21e4d643649686a8688acdbb
mikeatthebass <miguelaguirreleon@gmail.com>
2021-05-24 16:09:25 -0500

    sss

--- html/mapa1.js
+++ html/mapa1.js
@@ -9,15 +9,17 @@ $(document).ready(function() {
             var FECHA_HORA=[];
             var LVL1=[];
             var VEL=[];
+            var PFINAL=[];
             console.log(data);
             for (var i in data) {
                 FECHA_HORA.push(data[i].FECHA_HORA);
                 LVL1.push(data[i].LVL1);
                 VEL.push(data[i].VEL);
                 TRYC.push([data[i].LAT,data[i].LON]);
+                PFINAL=[data[i].LAT,data[i].LON];
             }
             console.log("value,", TRYC);
-            var mymap = L.map('mapid').setView([-12.0632, -77.1126], 13);
+            var mymap = L.map('mapid').setView(PFINAL, 13);
           	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
           		maxZoom: 18,
           		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -27,8 +29,8 @@ $(document).ready(function() {
           		zoomOffset: -1
           	}).addTo(mymap);
 
-          	L.marker([-12.0632, -77.1126]).addTo(mymap)
-          	.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+          	L.marker(PFINAL).addTo(mymap)
+          	.bindPopup("<b>Ubicacion Final</b><br/>").openPopup();
       	     L.polygon(TRYC).addTo(mymap).bindPopup("Trayectoria Recorrida");
           	var popup = L.popup();
           	function onMapClick(e) {
