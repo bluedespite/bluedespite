163827b721aa697047cc83e96f22cfef98e74cf2
miguelaguirreleon@gmail.com <miguelaguirreleon@gmail.com>
2021-07-11 20:59:14 -0500

    m

--- GUI/Roraima_Comm.py
+++ GUI/Roraima_Comm.py
@@ -22,165 +22,6 @@ arduinos={'Latitude': '-12.063190', 'Longitude': '-77.112600', 'Velocity': '0', 
 df = pd.DataFrame({'ID':[], 'FECHA_HORA': [], 'ID_ESTACION': [],'ESTACION': [], 'ID_TANQUE':[],'TANQUE':[], 'PRODUCTO':[], 'DENSIDAD':[], 'TAG_SENSOR':[],'DESCRIPCION':[],'UM':[], 'RANGO_MIN':[], 'RANGO_MAX':[],'TIPO':[],'DIRECCION':[],'MASCARA':[],'PUERTO':[],'ID_COMM':[],'SERIAL':[],'LINEAR':[], 'LATITUD':[], 'LONGITUD':[],'VELOCIDAD':[],'MEASURE':[]})
 analogico=[0,0,0,0,0,0]
 
-class VentanaSenales(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        self.parent = parent
-        plt.style.use('dark_background')
-        self.frame_graficas = tk.Frame(self, bg="#6E6E6E")
-        self._figure_1, self._ax1 = plt.subplots()
-        self._figure_1_canvas = FigureCanvasTkAgg(
-            self._figure_1, master=self.frame_graficas
-            )
-        self._figure_2, self._ax2 = plt.subplots()
-        self._figure_2_canvas = FigureCanvasTkAgg(
-            self._figure_2, master=self.frame_graficas
-            )
-        self._figure_3, self._ax3 = plt.subplots()
-        self._figure_3_canvas = FigureCanvasTkAgg(
-            self._figure_3, master=self.frame_graficas
-            )
-        self.frame_graficas.grid_columnconfigure(0, weight=1, uniform="fig")
-        self.frame_graficas.grid_columnconfigure(1, weight=1, uniform="fig")
-        self.frame_graficas.grid_columnconfigure(2, weight=1, uniform="fig")
-        self._figure_1_canvas.get_tk_widget().grid(
-            row=0, column=0, padx=(10, 30), pady=(30, 30),
-            sticky="nsew"
-            )
-        self._figure_2_canvas.get_tk_widget().grid(
-            row=0, column=1, padx=(10, 30), pady=(30, 30),
-            sticky="nsew"
-            )
-        self._figure_3_canvas.get_tk_widget().grid(
-            row=0, column=2, padx=(10, 30), pady=(30, 30),
-            sticky="nsew"
-            )
-        self.frame_botones = tk.Frame(self, bg="#151515")
-        self.btn_iniciar = tk.Button(
-            self.frame_botones, bg="#7401DF", fg="#FFBF00",
-            activebackground="#8258FA", font=('Courier', 16),
-            text="Iniciar", command=self.iniciar_animacion
-            )
-        self.btn_pausar = tk.Button(
-            self.frame_botones, bg="#7401DF", fg="#FFBF00",
-            activebackground="#8258FA", font=('Courier', 16),
-            text="  Pausa  ", command=self.pausar_animacion, state=tk.DISABLED
-            )
-        self.btn_iniciar.pack(
-            side="left", padx=(100, 100), pady=(100, 100),
-            fill="y", expand=True
-            )
-        self.btn_pausar.pack(
-            side="left", padx=(100, 100), pady=(100, 100),
-            fill="y", expand=True
-            )
-        self._anim1 = None
-        self._anim2 = None
-        self._anim3 = None
-        self.frame_graficas.pack(fill="both", expand=True)
-        self.frame_botones.pack(fill="x")
-        self._init_axes()
-    def _init_axes(self):
-        self._ax1.set_title('Signal')
-        self._ax1.set_xlabel("Time")
-        self._ax1.set_ylabel("Amplitude")
-        self._ax1.set_xlim(0, 100)
-        self._ax1.set_ylim(-1, 1)
-        self._ax2.set_title('Signal2')
-        self._ax2.set_xlabel("Time")
-        self._ax2.set_ylabel("Amplitude")
-        self._ax2.set_xlim(0, 100)
-        self._ax2.set_ylim(-1, 1)
-        self._ax3.set_title('Signal3')
-        self._ax3.set_xlabel("Time")
-        self._ax3.set_ylabel("Amplitude")
-        self._ax3.set_xlim(0, 100)
-        self._ax3.set_ylim(-1, 1)
-
-
-    def iniciar_animacion(self):
-
-        def animate(values):
-            value=values
-            data.append(value)
-            lines.set_data(range(0, 100), data)
-            return lines
-
-        def animate2(values):
-            value=values
-            data2.append(value)
-            lines2.set_data(range(0, 100), data2)
-            return lines2
-
-        def animate3(values):
-            value=values
-            data3.append(value)
-            lines3.set_data(range(0, 100), data3)
-            return lines3
-
-        def data_gen():
-            for k in range(100):
-                t = k / 100
-                yield 0.5 * np.sin(40 * t) * np.exp(-2 * t)
-
-        def data_gen2():
-            for k in range(100):
-                t = k / 100
-                yield 0.5 * np.sin(60 * t)
-
-        def data_gen3():
-            for k in range(100):
-                t = k / 100
-                yield 0.5 * np.cos(60 * t)
-
-        if self._anim1 is None:
-            lines = self._ax1.plot([], [], color='#80FF00')[0]
-            lines2 = self._ax2.plot([], [], color='#80FF00')[0]
-            lines3 = self._ax3.plot([], [], color='#80FF00')[0]
-
-            data = collections.deque([0] * 100, maxlen=100)
-            data2 = collections.deque([0] * 100, maxlen=100)
-            data3 = collections.deque([0] * 100, maxlen=100)
-
-            self._anim1 = animation.FuncAnimation(
-                self._figure_1, animate, data_gen, interval=5
-                )
-            self._anim2 = animation.FuncAnimation(
-                self._figure_2, animate2, data_gen2, interval=5
-                )
-            self._anim3 = animation.FuncAnimation(
-                self._figure_3, animate3, data_gen3, interval=5
-                )
-
-            self._figure_1_canvas.draw()
-            self._figure_2_canvas.draw()
-            self._figure_3_canvas.draw()
-
-            self.btn_pausar.configure(state=tk.NORMAL)
-            self.btn_iniciar.configure(text="Detener")
-        else:
-            self._ax1.lines = []  
-            self._ax2.lines = []
-            self._ax3.lines = []
-            self.btn_pausar.configure(state=tk.DISABLED, text="  Pausa  ")
-            self.btn_iniciar.configure(text="Iniciar")
-            self._anim1 = self._anim2 = self._anim3 = None
-
-
-    def pausar_animacion(self):
-        if self.btn_pausar["text"] == "  Pausa  ":
-            self._anim1.event_source.stop()
-            self._anim2.event_source.stop()
-            self._anim3.event_source.stop()
-            self.btn_pausar.configure(text="Continuar")
-
-        else:
-            self._anim1.event_source.start()
-            self._anim2.event_source.start()
-            self._anim3.event_source.start()
-            self.btn_pausar.configure(text="  Pausa  ")
-
-
 
 def init_logger():
     FORMAT = ('%(asctime)s - %(threadName)s %(levelname)s %(module)s %(lineno)s %(message)s')
@@ -298,8 +139,6 @@ def linealization(var,linear):
     for i in range(len(linear1)):
         xx=linear1[i].split(':')
         xx1=linear1[i+1].split(':')
-        print(xx)
-        print(xx1)
         try:
             x1=float(xx[0])
             x2=float(xx1[0])
@@ -341,6 +180,4 @@ if __name__ == "__main__":
     hilo1.start()
     time.sleep(10)
     hilo2.start()
-    root = tk.Tk()
-    VentanaSeñales(root).pack(side="top", fill="both", expand=True)
-    root.mainloop()
+    VentanaSenales()
